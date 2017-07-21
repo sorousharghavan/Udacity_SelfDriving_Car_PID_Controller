cbddeb62d0b3dacadf1a54fb325e04ff55e3a7b9
sorousharghavan <soroush.arghavan@gmail.com>
2017-07-20 21:19:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# PID Controller
+### Self-Driving Car Engineer Nanodegree - _Term 2, Project 4_
+### By: **Soroush Arghavan**
+---
+
+_Build instructions are at the bottom of this page_
+
+## Introduction
+This project uses a PID controller to steer a simulator around a track autonomously.
+
+To see a video of the vehicle driving a lap around the track autonomously visit the link below:
+[Video link](https://youtu.be/rteyqHYAR4Q)
+
+## Reflection
+### The effect of the P, I, D component of the PID algorithm in implementation. Is it what you expected?
+The P controller determines how agile the controller is to respond to deviation from the set point. The larger the P gain is, the larger the steer value will be. Ideally, a very large P gain is desired for this project. However, by increasing the P gain, the car will overshoot and begin to oscillate out of control. This is expected since the vehicle will begin to deviate further and further from the set point, increasing the steer value each time. This is where the D controller becomes handy.
+
+The D controller will dampen the amplitude of the oscillatiosn and allow for larger P gains. Again, this is aligned with the expectations since large P gains will cause larger differentials in steer values.
+
+The I controller ensures that the vehicles remains close to the center of the track as even the slightest deviations will build up to a large steer value. However, since the simulator does not have any "friction" or dissipation of energy, even a small value of I gain will result in a slight oscillation about the set point (center of the track). This is not surprising since the I controller is usually used to overcome physical imperfections such as friction or bias.
+
+## Dependencies
+
+* cmake >= 3.5
+ * All OSes: [click here for installation instructions](https://cmake.org/install/)
+* make >= 4.1
+  * Linux: make is installed by default on most Linux distros
+  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
+  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
+* gcc/g++ >= 5.4
+  * Linux: gcc / g++ is installed by default on most Linux distros
+  * Mac: same deal as make - [install Xcode command line tools]((https://developer.apple.com/xcode/features/)
+  * Windows: recommend using [MinGW](http://www.mingw.org/)
+* [uWebSockets](https://github.com/uWebSockets/uWebSockets)
+  * Run either `./install-mac.sh` or `./install-ubuntu.sh`.
+
+* Simulator. You can download these from the [project intro page](https://github.com/udacity/self-driving-car-sim/releases) in the classroom.
+
+There's an experimental patch for windows in this [PR](https://github.com/udacity/CarND-PID-Control-Project/pull/3)
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make`
+4. Run it: `./pid`. 
+
---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <uWS/uWS.h>
+#include <iostream>
+#include "json.hpp"
+#include "PID.h"
+#include <math.h>
+
+// for convenience
+using json = nlohmann::json;
+
+// For converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
+std::string hasData(std::string s) {
+  auto found_null = s.find("null");
+  auto b1 = s.find_first_of("[");
+  auto b2 = s.find_last_of("]");
+  if (found_null != std::string::npos) {
+    return "";
+  }
+  else if (b1 != std::string::npos && b2 != std::string::npos) {
+    return s.substr(b1, b2 - b1 + 1);
+  }
+  return "";
+}
+
+int main()
+{
+  uWS::Hub h;
+
+  PID pid;
+  //Initialize the pid variable.
+  //pid.Init(0.2,0,0.01); //Too much oscillation.
+  //pid.Init(0.1,0,0.01); //incresing oscillation leading to destabilization
+  //pid.Init(0.1,0,0.1); //reaction time of the controller is reduced leading to faster compensation of oscillation.
+  //pid.Init(0.05,0,0.3); //car went on the shoulder but much more stable than before. need to increase kp to make controller faster.
+  //pid.Init(0.07,0,0.3); //less oscillation, car on shoulder
+  //pid.Init(0.055,0,0.7); //very little oscillation but car is lazy
+  //pid.Init(0.06,0,0.8); // car is still lazy and on the shoulder
+  //pid.Init(0.075,0,0.95); // car is still lazy and on the shoulder
+  //pid.Init(0.09,0,1.2); //car is a lot more agile but still hits the shoulders on sharp turns
+  //pid.Init(0.095,0,1.35); //car is a lot more agile but still hits the shoulders on sharp turns
+  //pid.Init(0.1,0,1.65);//car is a lot more agile but still hits the shoulders on sharp turns
+  //pid.Init(0.16,0,2); //almost perfect. add a bit of integral gain to centralize the car on the road
+  //pid.Init(0.16,0.0005,2); // too much overshoot
+  pid.Init(0.16,0.00025,2);
+
+  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+      auto s = hasData(std::string(data).substr(0, length));
+      if (s != "") {
+        auto j = json::parse(s);
+        std::string event = j[0].get<std::string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          double cte = std::stod(j[1]["cte"].get<std::string>());
+          double speed = std::stod(j[1]["speed"].get<std::string>());
+          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          double steer_value;
+
+		  //Update error using the PID controller
+		  pid.UpdateError(cte);
+		  //Set the steering value to the PID error
+		  steer_value = pid.TotalError();
+          
+          // DEBUG
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+          json msgJson;
+          msgJson["steering_angle"] = steer_value;
+          msgJson["throttle"] = 0.3;
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+          std::cout << msg << std::endl;
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+  });
+
+  // We don't need this since we're not using HTTP but if it's removed the program
+  // doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1)
+    {
+      res->end(s.data(), s.length());
+    }
+    else
+    {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
+
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
+
+  int port = 4567;
+  if (h.listen(port))
+  {
+    std::cout << "Listening to port " << port << std::endl;
+  }
+  else
+  {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+}
